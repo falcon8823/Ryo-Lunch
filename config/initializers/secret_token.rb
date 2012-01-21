1607505ec274d694c92382c8fd543d9e92914782
Hayato OKUMOTO <falcon@falconsrv.net>
2012-01-21 21:22:36 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RyoLunch::Application.config.secret_token = '24518f2319c44584ffc5a54bb7d71ecf4007953ccfbed311a31772c88fa65c8e639b107c29f809dde3cf2395763ae0e956043daaa05deaea744c8404abbbac0a'
