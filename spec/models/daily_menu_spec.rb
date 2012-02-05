b1df5cb48b604ebe9a10fbbcd6f975a84efe0818
Hayato OKUMOTO <falcon@falconsrv.net>
2012-02-05 17:32:44 +0900

    create spec for DailyMenu model

--- spec/models/daily_menu_spec.rb
+++ spec/models/daily_menu_spec.rb
@@ -1,5 +1,34 @@
+# encoding: utf-8
 require 'spec_helper'
 
 describe DailyMenu do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "正常データの場合" do
+    before :each do
+      @daily_menu = DailyMenu.new(
+        date: Date.today,
+        breakfast: "パン",
+        lunch: "ラーメン",
+        dinner: "カレーライス"
+      )
+    end
+
+    it "データは検証に成功し かつ 保存できること" do
+      @daily_menu.should be_valid
+    end
+  end
+
+  context "#date が重複した場合" do
+    before :each do
+      @date = Date.today
+      @daily_menu = DailyMenu.new(date: @date)
+      @daily_menu.save!
+      
+      @duplicated_menu = DailyMenu.new(date: @date)
+    end
+
+    it "検証に失敗し かつ 保存できないこと" do
+      @duplicated_menu.should_not be_valid
+      @duplicated_menu.save.should be_false
+    end
+  end
 end
