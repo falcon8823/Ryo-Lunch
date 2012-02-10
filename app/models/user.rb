2fd63acbb2228dc4d1ddc453104bd6903fd1186d
Hayato OKUMOTO <falcon@falconsrv.net>
2012-02-10 19:30:51 +0900

    add User model specs & some validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates :email,
+    uniqueness: true,
+    presence: true,
+    format: {with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/},
+    length: {maximum: 256}
 end
