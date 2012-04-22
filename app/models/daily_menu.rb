40cffa7676812bbeea38021e15d19afbf29f5099
Hayato OKUMOTO <falcon@falconsrv.net>
2012-04-22 15:27:42 +0900

    add today scope

--- app/models/daily_menu.rb
+++ app/models/daily_menu.rb
@@ -1,6 +1,7 @@
 class DailyMenu < ActiveRecord::Base
   # Scope
   default_scope order: 'daily_menus.date ASC'
+  scope :today, where(date: Date.today)
 
   # Validates
   validates :date, uniqueness: :date
