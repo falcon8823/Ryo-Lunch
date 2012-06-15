d69b2357f16218bfc0439e22bfa1b236a6a5371c
Hayato OKUMOTO <falcon@falconsrv.net>
2012-06-15 13:12:59 +0900

    fix atom

--- app/models/daily_menu.rb
+++ app/models/daily_menu.rb
@@ -3,16 +3,19 @@ class DailyMenu < ActiveRecord::Base
   scope :desc_by_date, order('daily_menus.date DESC')
   scope :asc_by_date, order('daily_menus.date DESC')
 
-  today = Proc.new { Date.today }
 
   # 現在から先のメニューすべて
   scope :from_now,
-    where('date >= ?', today).order('daily_menus.date ASC')
+    lambda { where('date >= ?', Date.today).order('daily_menus.date ASC') }
   
   # 14日前から2日後まで
   scope :for_rss,
-    where('date >= ? AND date <= ?', 14.days.ago(today), 2.days.from_now(today)).order('daily_menus.date DESC')
-
+    lambda {
+      where(
+        'date >= ? AND date <= ?', 14.days.ago(Date.today), 
+        2.days.from_now(Date.today)
+      ).order('daily_menus.date DESC')
+    }
   # Validates
   validates :date, uniqueness: :date
 
