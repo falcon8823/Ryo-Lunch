fea5aee59ff28073b2d6d6f8c047c12f4ca1d6df
Hayato OKUMOTO <falcon@falconsrv.net>
2013-04-09 00:01:48 +0900

    Implement admin daily_menus page

---
+++ app/assets/javascripts/admin/application.js.coffee
@@ -0,0 +1,4 @@
+//= require jquery
+//= require jquery_ujs
+//= require ../normal/bootstrap
+//= require_tree .
--- app/controllers/admin/daily_menus_controller.rb
+++ app/controllers/admin/daily_menus_controller.rb
@@ -1,19 +1,62 @@
+# encoding: utf-8
+
 class Admin::DailyMenusController < Admin::ApplicationController
+  before_filter :set_daily_menu, only: [ :edit, :update, :destroy ]
+
   def index
+    @daily_menus = DailyMenu.desc_by_date
   end
 
   def new
+    @daily_menu = DailyMenu.new
   end
 
   def create
+    @daily_menu = DailyMenu.new(params[:daily_menu])
+
+    if @daily_menu.save
+      if params[:to_index]
+        url = admin_daily_menus_url
+      elsif params[:to_new]
+        url = new_admin_daily_menu_url
+      else
+        url = edit_admin_daily_menu_url(@daily_menu)
+      end
+
+      redirect_to url, notice: "#{l @daily_menu.date}の献立を追加しました。"
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @daily_menu.update_attributes(params[:daily_menu])
+      if params[:to_index]
+        url = admin_daily_menus_url
+      elsif params[:to_new]
+        url = new_admin_daily_menu_url
+      else
+        url = edit_admin_daily_menu_url(@daily_menu)
+      end
+
+      redirect_to url, notice: "#{l @daily_menu.date}の献立を更新しました。"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @daily_menu.destroy
+
+    redirect_to admin_daily_menus_url, notice: "#{l @daily_menu.date}の献立を削除しました。"
+  end
+
+  private
+
+  def set_daily_menu
+    @daily_menu = DailyMenu.find(params[:id])
   end
 end
