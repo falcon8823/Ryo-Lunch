e876ad68ce555def837b463733fc4a88badd30fd
Hayato OKUMOTO <falcon@falconsrv.net>
2013-04-08 22:38:51 +0900

    Create admin files

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 RyoLunch::Application.routes.draw do
-  get "registration/activate"
+  namespace :admin do
+    resources :daily_menus, except: [ :show ]
+    resources :users, except: [ :show ]
+  end
 
+  get "registration/activate"
   get "registration/stopping"
 
   resources :daily_menus, only: [:index, :show]
 
-  root to: 'Welcome#top'
-
+  root to: 'welcome#top'
 end
