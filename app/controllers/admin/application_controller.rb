44b4de9f9a31a7b3ec2d431e915a960feefc0803
Hayato OKUMOTO <falcon@falconsrv.net>
2013-04-09 00:44:03 +0900

    Implement admin authentication

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,3 +1,16 @@
 class Admin::ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :digest_auth
+
+  private
+
+  # ダイジェスト認証
+  # Digest::MD5::hexdigest([username, 'ryo-lunch-admin', password].join(':'))
+  # で生成可能
+  def digest_auth
+    users = APP_CONFIG['admin_users']
+    authenticate_or_request_with_http_digest('ryo-lunch-admin') do |n|
+      users[n]
+    end
+  end
 end
